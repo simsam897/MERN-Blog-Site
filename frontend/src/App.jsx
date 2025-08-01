73aa430ec9de84ed9d54a22db1d0177122f47274
simsam897 <simma013s@gmail.com>
2025-08-01 23:24:52 +0530

    react vite and tailwind installation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-2xl text-red-600 font-bold '>blog site</h1>
+    </>
+  )
+}
+
+export default App
