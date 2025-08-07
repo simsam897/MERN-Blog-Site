324e5f43aa0349ed66296776440c5096dc5f42e1
simsam897 <simma013s@gmail.com>
2025-08-07 21:36:29 +0530

    create user model

---
+++ backend/Models/user.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+
+
+  {
+
+  username:{
+    type:String,
+    required:true,
+    unique:true,
+    lowercase:true
+
+  },
+
+  email: {
+    type:String,
+    required:true,
+    unique:true,
+    lowercase:true
+  },
+
+  
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+      maxLength: 16,
+    },
+
+
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
